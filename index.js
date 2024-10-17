896a75136ba06a9d7e292e1198c37e106ccafcd2
tee101 <akpata.thelma@gmail.com>
2024-10-17 17:53:12 +0100

    dashboard card implementation

---
+++ index.js
@@ -0,0 +1,34 @@
+// script.js
+document.querySelector('.dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
+  
+  // Optional dark mode styling
+  const darkModeStyles = `
+    .dark-mode {
+      background-color: #333;
+      color: #fff;
+    }
+  
+    .dark-mode .sidebar {
+      background-color: #444;
+    }
+  
+    .dark-mode .card {
+      background-color: #555;
+    }
+  
+    .dark-mode .status.completed {
+      background-color: #218838;
+    }
+  
+    .dark-mode .status.in-progress {
+      background-color: #e0a800;
+    }
+  `;
+  
+  const styleSheet = document.createElement('style');
+  styleSheet.type = 'text/css';
+  styleSheet.innerText = darkModeStyles;
+  document.head.appendChild(styleSheet);
+  
